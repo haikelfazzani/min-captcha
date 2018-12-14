e7f48ca4bdf613c0e2c01028ac20cd07b4f0ff73
Haikel Fazzani <haikel.fazzani@gmail.com>
2018-12-14 14:49:13 +0100

    update readme, add comments to methods & update examples

--- examples/app.js
+++ examples/app.js
@@ -1,19 +1,30 @@
-import { Captcha } from '../src/api';
+import { Captcha } from '../src/captcha';
 
 window.onload = () => {
 
-    let input = document.getElementById("input") ,
-        btn = document.getElementById("btn") ,
-        result = document.getElementById("result") ,
-        myCanvas = document.getElementById("my-canvas");
+    let input       = document.getElementById("input") ,
+        btn         = document.getElementById("btn") ,
+        result      = document.getElementById("result") ,
+        myCanvas    = document.getElementById("my-canvas");
 
-    let captcha = new Captcha();        
+    // first : get instance from Captcha class
+    let captcha = new Captcha(); 
+
+    /* 
+        Second : get a random string using getRndString method , 
+        you can specify how many character (nbChar : optionnal) it will be displayed into the canvas element
+        by default : nbChar = 5
+    */
     let randString = captcha.getRndString({nbChar:3});
 
-    console.log(randString);
-
+    /* 
+        third : setup canvas using the setupCanvas method and add the random string generated as parameter
+        randString : optionnal
+    */
     let canvas = captcha.setupCanvas({randString});
+    // last : append canvas to any element you want
     myCanvas.appendChild(canvas);
+
 
     result.style.display = "none";
     btn.onclick = () => {
